4eff63c530726b54428a2d9574ce3159a67e886a
Marko Rankovic <markocrush96@gmail.com>
2020-10-06 19:19:16 +0100

    Initial Commit

---
+++ Asteroid Watch Login/Asteroid_Watch_LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  Asteroid_Watch_LoginApp.swift
+//  Asteroid Watch Login
+//
+//  Created by Marko Rankovic on 10/6/20.
+//
+
+import SwiftUI
+
+@main
+struct Asteroid_Watch_LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
